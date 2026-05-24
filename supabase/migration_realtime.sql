eadf7898569a8077726a0474352d541c7cf0d12a
bonobo85 <exon.wizard@gmail.com>
2026-05-24 18:27:35 +0200

    del

--- supabase/migration_realtime.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Enable Supabase Realtime on announcements table
--- Run this in the Supabase SQL Editor
-
--- Enable realtime for announcements
-ALTER PUBLICATION supabase_realtime ADD TABLE announcements;
